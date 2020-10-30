21a9de1b9ae717d4907facae83d38010948abb41
Connor Barker <connorlbark@gmail.com>
2020-10-30 18:57:31 -0400

    transition back to vue2; get formulate and tailwind working

---
+++ src/components/TestForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+
+        <FormulateForm
+            v-model="formValues"
+            >
+            <FormulateInput
+                name="name"
+                label="What is your name?"
+                validation="required"
+            />
+            <FormulateInput
+                name="age"
+                label="How old are you?"
+                validation="required|number|max:120"
+            />
+            <FormulateInput
+                type="submit"
+                label="Sign up"
+            />
+        </FormulateForm>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'test-form',
+    data: () => {
+        return {
+            formValues: {},
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
